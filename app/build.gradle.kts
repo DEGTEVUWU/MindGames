3cc0bf902737d7c8da424ab5762fa62e3040007b
ivan dyogtev <diogteff.ivan@yandex.ru>
2023-12-01 14:53:49 +0200

    3, new makefile + move groove -> kotlin, new settings.kts, build.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    application
+    checkstyle
+}
+application{
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+    }
+}
+val run by tasks.getting(JavaExec::class) {
+    standardInput = System.`in`
+}
