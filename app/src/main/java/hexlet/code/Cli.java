376a79cb3027a0cd235c253cb29bb091eba8298c
ivan dyogtev <diogteff.ivan@yandex.ru>
2023-12-01 19:29:07 +0200

    4, App - new code, new Class - Cli

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+    public static String console() {
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+
+        return userName;
+    }
+
+}
