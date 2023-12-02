3211b88ab2062e2fb444615a5f40124d3b17f143
ivan dyogtev <diogteff.ivan@yandex.ru>
2023-12-02 10:55:29 +0200

    8, choose App & Cli classes for output greeting, game-menu with 1,0 value

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,22 @@
 package hexlet.code;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Welcome to the Brain Games!");
-        Cli.console();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        String userChoice = scan.next();
+        if(userChoice.equals("1")) {
+            Cli.console();
+        }
+        //Choice.choice();
+
+
+
+        //System.out.println("Welcome to the Brain Games!");
+        //Cli.console();
     }
 }
