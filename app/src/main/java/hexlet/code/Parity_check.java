d0c5aa1a7c082b52b60bc70e5cc18ac74a6c3a27
ivan dyogtev <diogteff.ivan@yandex.ru>
2023-12-02 13:29:29 +0200

    10, choose code syntax in class Parity-check fot more readability in code

--- app/src/main/java/hexlet/code/Parity_check.java
+++ app/src/main/java/hexlet/code/Parity_check.java
@@ -19,26 +19,31 @@ public class Parity_check {
             System.out.print("Your answer: ");
             Scanner scanner1 = new Scanner(System.in);
             String answer1 = scanner1.next();
+            String wrongAmswer = " is wrong answer ;(. Correct answer was ";
+            String wrongAnswer2srting = "Let's try again, ";
+
             if (number % 2 == 0 && answer1.equals("yes")) {
                 System.out.println("Correct!");
             } else if (number % 2 == 1 && answer1.equals("no")) {
                 System.out.println("Correct!");
             } else if (number % 2 == 0 && answer1.equals("no")) {
-                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
-                System.out.println("Let's try again, " + userName);
+                System.out.println(answer1 + wrongAmswer + "'yes'.");
+                System.out.println(wrongAnswer2srting + userName);
                 break;
             } else if (number % 2 == 1 && answer1.equals("yes")) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                System.out.println("Let's try again, " + userName);
+                System.out.println(answer1 + wrongAmswer + "'no'.");
+                System.out.println(wrongAnswer2srting + userName);
                 break;
             }
             else {
                 if(number % 2 == 0) {
-                    System.out.println("'" + answer1 + "'" + " is wrong answer ;(. Correct answer was 'yes'.");
+                    System.out.println("'" + answer1 + "'" + wrongAmswer + "'yes'.");
+                    System.out.println(wrongAnswer2srting + userName);
                     break;
                 }
                 if(number % 2 == 1) {
-                    System.out.println("'" + answer1 + "'" + " is wrong answer ;(. Correct answer was 'no'.");
+                    System.out.println("'" + answer1 + "'" + wrongAmswer + "'no'.");
+                    System.out.println(wrongAnswer2srting + userName);
                     break;
                 }
             }
