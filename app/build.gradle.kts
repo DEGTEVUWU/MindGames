44d30d618368ed156db1fc97452cda2b74a74cb8
ivan dyogtev <diogteff.ivan@yandex.ru>
2023-12-06 12:36:10 +0200

    31, change 2 files - Makefile and build.gradle.kts for work Code Climate-tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,25 +1,45 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     application
-    checkstyle
-}
-application{
-    mainClass = "hexlet.code.App"
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.4"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+application { mainClass.set("hexlet.code.App") }
+
+repositories { mavenCentral() }
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
     }
 }
-val run by tasks.getting(JavaExec::class) {
-    standardInput = System.`in`
-}
-tasks.getByName("run", JavaExec::class) {
-    standardInput = System.`in`
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
 }
